99f843dad248df6cee8d28e3f97b109836f5a435
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-07-09 14:43:00 -0700

    completed exercises

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,23 @@
 # Make up three entries of various values.
 
 waypoints = [
+    {
+        "lat": 42,
+        "lon": 17,
+        "name": "hobbiton"
+    },
+    {
+        "lat": 13,
+        "lon": 2,
+        "name": "isengard"
+    },
+    {
+        "lat": 54,
+        "lon": 13,
+        "name": "mordor"
+    }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for w in waypoints:
+    print(w['name'], w['lat'], w['lon'])
